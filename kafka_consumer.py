dd85c1ee0b1a672677f2b8b2be7a67c4728931ea
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2022-04-04 21:41:59 +0300

    added some bug fixes

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,5 +1,4 @@
-import json 
-from pprint import pprint
+import json
 from datetime import datetime
 from data_development import (
     check_value_file,
@@ -21,18 +20,16 @@ for message in consumer:
     message_sent_uuid = message_sent.get('uuid', False)
     message_sent_date = message_sent.get('date_created', 'unknown')
     if message_sent.get('uuid'):
-        message_check = {message_sent_uuid:message_sent_date}
         if not develop_file_check(message_sent_uuid):
             
             received = datetime.utcnow()
             send = datetime.strptime(message_sent.get('date_created'), '%Y-%m-%d %H:%M:%S.%f')
             delta = received - send
-            delta_used = delta.microseconds/1000000 + delta.seconds + delta.days*60*60*24 
             value_required = {
                 "uuid": message_sent.get('uuid'),
                 'date_send': message_sent.get('date_created'),
                 'date_received': str(received),
-                'delta': delta_used
+                'delta': delta.microseconds/1000000 + delta.seconds + delta.days*60*60*24 
             }    
             develop_file_insert(
                 value_required, 
@@ -44,5 +41,5 @@ for message in consumer:
                 }, 
                 False
             )
-            print(message_check)
-            print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+            print(message_sent.get("index", -1), message_sent_uuid, message_sent_date)
+            print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
--- kafka_producer.py
+++ kafka_producer.py
@@ -1,7 +1,5 @@
-from pprint import pprint
-from time import sleep
 from json import dumps
-from pprint import pprint
+from datetime import datetime
 from kafka import KafkaProducer
 from data_development import develop_random_data
 
@@ -11,7 +9,13 @@ producer = KafkaProducer(
     value_serializer=lambda x: dumps(x).encode('utf-8')
 )
 
+print('Started at: ', datetime.utcnow())
+print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+
 for j in range(1, 100000):
     data = develop_random_data(j)
     print(f"Sent parameter with index:", j)
     producer.send('message', value=data)
+
+print('Finished at: ', datetime.utcnow())
+print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
