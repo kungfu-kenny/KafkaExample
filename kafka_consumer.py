195a3564981b3f7a1c3c5a0e77029071d61061d6
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2022-04-13 18:30:05 +0300

    developed consumer as one function

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -8,6 +8,37 @@ from kafka import KafkaConsumer
 from config import topic_test, value_add_check
 
 
+def calculate_consumer(received, message_sent, message_sent_uuid, message_sent_date):
+    proccessed = datetime.utcnow()
+    send = datetime.strptime(message_sent.get('date_created'), '%Y-%m-%d %H:%M:%S.%f')
+    delta_full = proccessed - send
+    delta_proccessed = proccessed - received
+    delta_send = received - send
+    value_required = {
+        "uuid": message_sent.get('uuid'),
+        "name_person": message_sent.get("name_person"),
+        'date_send': message_sent.get('date_created'),
+        'date_received': received.strftime('%Y-%m-%d %H:%M:%S.%f'),
+        'date_processed': proccessed.strftime('%Y-%m-%d %H:%M:%S.%f'),
+        'delta_send': delta_send.microseconds/1000000 + delta_send.seconds + delta_send.days*60*60*24,
+        'delta_full': delta_full.microseconds/1000000 + delta_full.seconds + delta_full.days*60*60*24,
+        'delta_proccessed': delta_proccessed.microseconds/1000000 + delta_proccessed.seconds + delta_proccessed.days*60*60*24, 
+    }    
+    develop_file_insert(
+        value_required, 
+        True,
+        topic_test
+    )
+    develop_file_insert(
+        {
+            message_sent_uuid:message_sent_date
+        }, 
+        False,
+        topic_test
+    )
+    print(message_sent.get("index", -1), message_sent_uuid, message_sent_date)
+    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')        
+
 consumer = KafkaConsumer(
     topic_test,
     bootstrap_servers='localhost:9092',
@@ -20,59 +51,20 @@ for message in consumer:
     message_sent_date = message_sent.get('date_created', 'unknown')
     if value_add_check and message_sent.get('uuid'):
         if not develop_file_check_new(topic_test, message_sent_uuid):
-            proccessed = datetime.utcnow()
-            send = datetime.strptime(message_sent.get('date_created'), '%Y-%m-%d %H:%M:%S.%f')
-            delta_full = proccessed - send
-            delta_proccessed = proccessed - received
-            delta_send = received - send
-            value_required = {
-                "uuid": message_sent.get('uuid'),
-                "name_person": message_sent.get("name_person"),
-                'date_send': message_sent.get('date_created'),
-                'date_received': str(received),
-                'date_processed': str(proccessed),
-                'delta_send': delta_send.microseconds/1000000 + delta_send.seconds + delta_send.days*60*60*24,
-                'delta_full': delta_full.microseconds/1000000 + delta_full.seconds + delta_full.days*60*60*24,
-                'delta_proccessed': delta_proccessed.microseconds/1000000 + delta_proccessed.seconds + delta_proccessed.days*60*60*24, 
-            }    
-            develop_file_insert(
-                value_required, 
-                True,
-                topic_test
+            calculate_consumer(
+                received,
+                message_sent,
+                message_sent_uuid,
+                message_sent_date
             )
-            develop_file_insert(
-                {
-                    message_sent_uuid:message_sent_date
-                }, 
-                False,
-                topic_test
-            )
-            print(message_sent.get("index", -1), message_sent_uuid, message_sent_date)
-            print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
     elif value_add_check and not message_sent.get('uuid'):
         print('Strange Message:')
         print(message_sent)
         print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
     else:
-        proccessed = datetime.utcnow()
-        send = datetime.strptime(message_sent.get('date_created'), '%Y-%m-%d %H:%M:%S.%f')
-        delta_full = proccessed - send
-        delta_proccessed = proccessed - received
-        delta_send = received - send
-        value_required = {
-            "uuid": message_sent.get('uuid'),
-            "name_person": message_sent.get("name_person"),
-            'date_send': message_sent.get('date_created'),
-            'date_received': str(received),
-            'date_processed': str(proccessed),
-            'delta_send': delta_send.microseconds/1000000 + delta_send.seconds + delta_send.days*60*60*24,
-            'delta_full': delta_full.microseconds/1000000 + delta_full.seconds + delta_full.days*60*60*24,
-            'delta_proccessed': delta_proccessed.microseconds/1000000 + delta_proccessed.seconds + delta_proccessed.days*60*60*24, 
-        }    
-        develop_file_insert(
-            value_required, 
-            True,
-            topic_test
+        calculate_consumer(
+            received,
+            message_sent,
+            message_sent_uuid,
+            message_sent_date
         )
-        print(message_sent.get("index", -1), message_sent_uuid, message_sent_date)
-        print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
